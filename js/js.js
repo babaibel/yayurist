b3a16d6c8ffbf922020883b816c19583453e4159
Babaev-2010 <babaev-2010@mail.ru>
2017-06-20 19:28:21 +0300

    на сервер. вариант1

--- js/js.js
+++ js/js.js
@@ -13,8 +13,8 @@ $(function() {
 		showCloseBtn: false,
 		mainClass: 'popup-modal-overlay',
 		callbacks: {
-			open: function() { $('._scrolled .header-wr').css('padding-right', swidth + "px"); }, 
-			close: function() { $('._scrolled .header-wr').css('padding-right', 0); },
+			open: function() { $('.header-wr').css('padding-right', swidth + "px"); }, 
+			close: function() { $('.header-wr').css('padding-right', 0); },
 		}
 	});
 
@@ -22,6 +22,66 @@ $(function() {
 		e.preventDefault();
 		$.magnificPopup.close();
 	});
+
+	$('#select-beast').selectize({
+	    create: true,
+	    sortField: 'text'
+	});
 });
 
+$(function() {
 
+	$('.js-form').submit(function () {
+		var $form = $(this),
+			title = $form.data('title'),
+			text = $form.data('text'),
+			$popup = $('#after-modal'),
+			$popupTitle = $popup.find('.js-popup-title'),
+			$popupText= $popup.find('.js-popup-text');
+
+		//проверка на успешную отправку
+
+		$popupTitle.text(title);
+		$popupText.text(text);
+
+		$.magnificPopup.close();
+		$.magnificPopup.open({
+			showCloseBtn: false,
+			callbacks: {
+				open: function() { $('.header-wr').css('padding-right', swidth + "px"); }, 
+				close: function() { $('.header-wr').css('padding-right', 0); },
+			},
+			items: {
+				src: '#after-modal'
+			},
+			type: 'inline'
+			}, 
+		0);
+		return false;
+	});
+
+});
+
+$(function() {
+
+	var $tab = $('.js-tab');
+
+	if(!$tab.length) return;
+
+	var $tabBtns = $tab.find('.js-btns > div'),
+		$tabTabs = $tab.find('.js-tabs > div');
+
+	function tab(numb) {
+		$tabTabs.removeClass('active')
+			.eq(numb).addClass('active');
+
+		$tabBtns.removeClass('active')
+			.eq(numb).addClass('active');
+	}
+
+	$tabBtns.bind('click', function () {
+		var index = $(this).index();
+		tab(index);
+	});
+
+});
